c2198623c241ecbdb79b8167bbbc08933a7afbb9
SSIM <ssim@SSIMui-Macmini.local>
2025-06-02 12:10:44 +0900

    good

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,22 @@
 const swiper = new Swiper('.visual-swiper--section .swiper', {
-    loop: true,
-  
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+const newSwiper = new Swiper('.news_content--block .swiper', {
+  slidesPerView: 2,
+  spaceBetween: 20,
+});
