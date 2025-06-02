74e757a21aad3578d7c218d9c47aecbe1a903d13
SSIM <ssim@SSIMui-Macmini.local>
2025-06-02 15:57:49 +0900

    good

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,3 +20,21 @@ const newSwiper = new Swiper('.news_content--block .swiper', {
   slidesPerView: 2,
   spaceBetween: 20,
 });
+const mediaSwiper = new Swiper('.media_section .swiper', {
+  slidesPerView: 4,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.media_section .swiper-button-next',
+    prevEl: '.media_section .swiper-button-prev',
+  },
+});
+
+const photoSwiper = new Swiper('.photo_section .swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 40,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
