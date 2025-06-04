6fdf61a4de9524ef75ed77e07a2a0a7beb14de0f
SSIM <ssim@SSIMui-Macmini.local>
2025-06-04 10:08:16 +0900

    good

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,16 @@
 const swiper = new Swiper('.visual-swiper--section .swiper', {
   loop: true,
-
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  scrollbar: {
-    el: '.swiper-scrollbar',
+    nextEl: '.visual-swiper--section .swiper-button-next',
+    prevEl: '.visual-swiper--section .swiper-button-prev',
   },
 });
-
-
+const campswiper = new Swiper('.camp_swiper_mobile.swiper', {
+  navigation: {
+    nextEl: '.camp_swiper_mobile .swiper-button-next',
+    prevEl: '.camp_swiper_mobile .swiper-button-prev',
+  },
+});
 const newSwiper = new Swiper('.news_content--block .swiper', {
   slidesPerView: 2,
   spaceBetween: 20,
@@ -34,7 +29,48 @@ const photoSwiper = new Swiper('.photo_section .swiper', {
   spaceBetween: 40,
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.photo_section .swiper-button-next',
+    prevEl: '.photo_section .swiper-button-prev',
   },
+});
+
+
+document.querySelector("#GoToTop").addEventListener("click", function () {
+  gsap.to(window, {
+    scrollTo: 0,
+    duration: 1.5,
+    ease: "power2.out"
+  });
+});
+
+
+
+
+
+
+
+document.querySelectorAll(".mobilenav--block .nav_item a").forEach(function (el) {
+  el.addEventListener("click", function () {
+    document.querySelectorAll(".mobilenav--block .nav_item a").forEach(function (link) {
+      link.classList.remove("active");
+    });
+
+    this.classList.add("active");
+  });
+});
+
+
+const mm = gsap.matchMedia();
+// pc
+mm.add("(min-width:1420)", function () {
+ 
+});
+// mob
+mm.add("(max-width:1020)", function () {
+  const campswiper = new Swiper('.camp_swiper_mobile.swiper', {
+    navigation: {
+      nextEl: '.camp_swiper_mobile .swiper-button-next',
+      prevEl: '.camp_swiper_mobile .swiper-button-prev',
+    },
+  });
 });
